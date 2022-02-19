314da7ce5c9e79761bb544522b555e59ba3ce0cb
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 11:32:53 +0100

    Removed express import from root index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
-const express = require("express");
 const debug = require("debug")("myRobots:root");
 const chalk = require("chalk");
 const initializeServer = require("./server");
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 (async () => {
