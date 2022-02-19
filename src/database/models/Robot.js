959173833697c4e26ceb6e36fe86c730dc6f7cf3
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 13:48:54 +0100

    Added validation rules to robotSchema

--- src/database/models/Robot.js
+++ src/database/models/Robot.js
@@ -5,6 +5,28 @@ const robotSchema = new Schema({
     type: String,
     required: true,
   },
+  speed: {
+    type: Number,
+    min: [0, "speed can't be less than 0, got {VALUE}"],
+    max: [10, "speed can't be more than 10, got {VALUE}"],
+    required: true,
+  },
+  endurance: {
+    type: Number,
+    min: [0, "endurance can't be less than 0, got {VALUE}"],
+    max: [10, "endurance can't be more than 10, got {VALUE}"],
+    required: true,
+  },
+  creationDate: {
+    type: String,
+    validate: {
+      validator(v) {
+        return /\d{2}-\d{2}-\d{1,}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid date format`,
+    },
+    required: true,
+  },
 });
 
 const Robot = model("Robot", robotSchema, "robots");
