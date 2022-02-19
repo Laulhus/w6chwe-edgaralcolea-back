6176ee40afb817b0fa2f5210872865b8f92f7484
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 14:36:05 +0100

    Added middleware with instructions when accessing root path

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ const debug = require("debug")("myRobots:server");
 const chalk = require("chalk");
 const morgan = require("morgan");
 const { default: helmet } = require("helmet");
+const { response } = require("express");
 const { generalError, notFoundError } = require("./middlewares/errors");
 const robotsRouter = require("./routers/robotsRouter");
 
@@ -21,7 +22,15 @@ const initializeServer = (port) =>
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
+app.get("/", (req, res, next) => {
+  res.status(200);
+  res.write(`Welcome to Ed's Robots
+  
+  Here are the endpoints you can use:
 
+  GET: /robots/all -- Shows a list of all robots`);
+  res.end();
+});
 app.use("/robots", robotsRouter);
 
 app.use(notFoundError);
