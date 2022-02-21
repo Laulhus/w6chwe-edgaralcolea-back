7d558b5c6fcb80c1b9cec9182f12a0ebee939a6b
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-21 18:14:49 +0100

    Created loginUser controller

--- src/controllers/loginUser.js
+++ src/controllers/loginUser.js
@@ -1,3 +1,24 @@
-const loginUser = (req, res, next) => {
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { default: User } = require("../database/models/User");
+
+const loginUser = async (req, res, next) => {
   const { userName, password } = req.body;
+  const user = await User.findOne({ userName });
+  if (!user) {
+    const error = new Error("User not found");
+    next(error);
+  } else {
+    const rightPassword = await bcrypt.compare(password, user.password);
+    if (!rightPassword) {
+      const error = new Error("Wrong credentials");
+      next(error);
+    } else {
+      const userData = { name: userName, id: user.id };
+      const token = jwt.sign(userData, process.env.JWT_SECRET);
+      res.json({ token });
+    }
+  }
 };
+
+export default loginUser;
