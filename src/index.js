8efc908631f5efadf9c0acfc4756b0e37331e186
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-21 17:37:38 +0100

    Installed bcrypt and encrypted password

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const debug = require("debug")("myRobots:root");
 const chalk = require("chalk");
+const bcrypt = require("bcrypt");
 const connectDatabase = require("./database");
 const initializeServer = require("./server");
 
@@ -15,4 +16,9 @@ const databasebUrl = process.env.DB_URL;
   } catch (error) {
     debug(chalk.redBright(`Error: ${error.message}`));
   }
+})();
+
+(async () => {
+  const encryptedPassword = await bcrypt.hash("olakease", 10);
+  debug(encryptedPassword);
 })();
