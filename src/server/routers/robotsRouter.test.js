b29682eed61577bc49a69ff2c4e096fe6c04ba51
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-22 23:42:36 +0100

    Tested DELETE robots/delete requests

--- src/server/routers/robotsRouter.test.js
+++ src/server/routers/robotsRouter.test.js
@@ -1,5 +1,24 @@
+const request = require("supertest");
+const app = require("../index");
+
 describe("Given a robotsRouter router", () => {
   describe("When it receives a DELETE request without token", () => {
-    test("Then it should call next with an error", () => {});
+    test("Then it should call next with an error with the message 'Token missing'", async () => {
+      const errorMessage = "Token missing";
+      const { body } = await request(app)
+        .delete("/robots/delete/4")
+        .expect(401);
+      expect(body.message).toBe(errorMessage);
+    });
+  });
+
+  describe("When it receives a POST request with a valid token", () => {
+    test("Then it should call the response json method with a robot", async () => {
+      const errorMessage = "Token missing";
+      const { body } = await request(app)
+        .delete("/robots/delete/4")
+        .expect(401);
+      expect(body.message).toBe(errorMessage);
+    });
   });
 });
